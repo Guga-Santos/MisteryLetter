08aa8270df0118ab16b4d5eba77d45c37f3a9e59
GugaSantos <guga.santos.guga@gmail.com>
2021-12-13 00:03:36 -0300

    Requisito 16 e 18 ok. Esse requisito 16 e 17 estao acabando com meu codigo!

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const criarButton = document.querySelector('#criar-carta');
 const cartaGerada = document.querySelector('#carta-gerada');
-const classeEstilo = [' newspaper', ' magazine1', ' magazine2'];
-const classeTamanho = [' medium', ' big', ' reallybig'];
-const classeRotacao = [' rotateleft', 'rotateright'];
-const classeInclinacao = [' skewleft', ' skewright'];
+const todasClasses = ['newspaper',
+'magazine1',
+'magazine2',
+'medium',
+'big',
+'reallybig',
+'rotateleft',
+'rotateright',
+'skewleft',
+'skewright'];
+// const classeEstilo = [' newspaper ', ' magazine1 ', ' magazine2 ', '', ' magazine3 ', ' magazine4 ', ' magazine5 '];
+// const classeTamanho = [' medium ', ' big ', ' reallybig ', ''];
+// const classeRotacao = [' rotateleft ', ' rotateright ', ''];
+// const classeInclinacao = [' skewleft ', ' skewright ', ''];
 
-function numeroAleatorio() {
-    const numero = Math.floor(Math.random() * 4)
-    return numero
+function classeAlea(classe, tamanho) {
+  const numero = Math.floor(Math.random() * tamanho);
+  return classe[numero];
 }
 
+function numeroAleatorio() {
+  const numero = Math.floor(Math.random() * 10);
+  return numero;
+}
 
 criarButton.addEventListener('click', () => {
   cartaGerada.innerHTML = '';
@@ -21,18 +35,27 @@ criarButton.addEventListener('click', () => {
     const array = texto.split(' ');
     for (let i = 0; i < array.length; i += 1) {
       const span = document.createElement('span');
+      span.appendChild(document.createTextNode(array[i]));
+    //   span.innerText = array[i];
+      const teste = Math.floor(Math.random() * todasClasses.length);
+      const novaClasse = todasClasses[teste]
+      span.classList.add(novaClasse)
       cartaGerada.appendChild(span);
-      span.innerText = array[i];
-      span.classList += ' ' + classeEstilo[numeroAleatorio()]
-      span.classList += ' ' + classeTamanho[numeroAleatorio()]
-      span.classList += ' ' + classeRotacao[numeroAleatorio()]
-      span.classList += ' ' + classeInclinacao[numeroAleatorio()]
+    //   const listaDeClasses = [classeAlea(classeEstilo, 7), classeAlea(classeTamanho, 4), classeAlea(classeRotacao, 3), classeAlea(classeInclinacao, 3)];
+    //   span.classList += listaDeClasses;
     }
   }
-  
+  document.querySelector('#carta-contador').innerText = cartaGerada.childElementCount;
 });
 
 const cartaTexto = document.querySelector('#carta-texto');
 cartaTexto.addEventListener('click', () => {
-    document.querySelector('#carta-texto').value = '';
-})
+  document.querySelector('#carta-texto').value = '';
+});
+// const teste2 =  + " " + classeAlea(todasClasses, 9)
+// const teste3 = 
+
+cartaGerada.addEventListener('click', (event) => {
+  event.target.classList = '';
+  event.target.classList += classeAlea(todasClasses, 9)
+});
