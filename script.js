5235c9566fe97953f5275f8e8a0545ab6b210610
GugaSantos <guga.santos.guga@gmail.com>
2021-12-12 21:42:12 -0300

    Requisito 04 ok - Requisitos do 01 ao 15 ok

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const criarButton = document.querySelector('#criar-carta');
 const cartaGerada = document.querySelector('#carta-gerada');
+const classeEstilo = [' newspaper', ' magazine1', ' magazine2'];
+const classeTamanho = [' medium', ' big', ' reallybig'];
+const classeRotacao = [' rotateleft', 'rotateright'];
+const classeInclinacao = [' skewleft', ' skewright'];
+
+function numeroAleatorio() {
+    const numero = Math.floor(Math.random() * 4)
+    return numero
+}
+
 
 criarButton.addEventListener('click', () => {
-    cartaGerada.innerHTML = ''
-    const texto = document.querySelector('#carta-texto').value
-    if(texto.trim() === '') {
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trim
-        cartaGerada.innerText = "Por favor, digite o conteúdo da carta."
-    } else {
-    const array = texto.split(" ")
-    for(let i = 0; i < array.length; i += 1) {
-        let span = document.createElement('span');
-        cartaGerada.appendChild(span)
-        span.innerText = array[i]
+  cartaGerada.innerHTML = '';
+  const texto = document.querySelector('#carta-texto').value;
+  if (texto.trim() === '') {
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    const array = texto.split(' ');
+    for (let i = 0; i < array.length; i += 1) {
+      const span = document.createElement('span');
+      cartaGerada.appendChild(span);
+      span.innerText = array[i];
+      span.classList += ' ' + classeEstilo[numeroAleatorio()]
+      span.classList += ' ' + classeTamanho[numeroAleatorio()]
+      span.classList += ' ' + classeRotacao[numeroAleatorio()]
+      span.classList += ' ' + classeInclinacao[numeroAleatorio()]
     }
-    }
-    document.querySelector('#carta-texto').value = ''
-})
+  }
+  
+});
 
+const cartaTexto = document.querySelector('#carta-texto');
+cartaTexto.addEventListener('click', () => {
+    document.querySelector('#carta-texto').value = '';
+})
